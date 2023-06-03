8db35a707d8c818074c4217febb062629d0f2d9c
Aniebietabasi <aniebietabasiobo@gmail.com>
2023-06-03 13:53:39 +0100

    commit

--- screens/Chats.js
+++ screens/Chats.js
@@ -124,7 +124,7 @@ export default function Chats() {
             </>
           )}
         </View>
-        <FloatingMesageIcon />
+        {/* <FloatingMesageIcon /> */}
       </Container>
     </SafeAreaView>
   );

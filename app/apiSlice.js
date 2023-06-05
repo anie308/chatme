28396d529f811dd69e0b5cee80975b77c71b4348
Adophilus <uchenna19of@gmail.com>
2023-06-05 21:44:49 +0100

    updated the path

--- app/apiSlice.js
+++ app/apiSlice.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
-const PROD = 'https://chatwave.onrender.com/api/v1'
+// const PROD = 'https://chatwave.onrender.com/api/v1'
+const PROD = "http://172.16.14.13:5000/api/v1";
 export const apiSlice = createApi({
-    reducerPath: 'apiSlice',
-    baseQuery: fetchBaseQuery({ baseUrl: PROD}),
+    reducerPath: "apiSlice",
+    baseQuery: fetchBaseQuery({ baseUrl: PROD }),
     keepUnusedDataFor: 10,
-    tagTypes: ['messages'],
-    endpoints:(builder) => ({})
-})
+    tagTypes: ["messages"],
+    endpoints: (builder) => ({}),
+});
